e1f0cb5e7204ef68f272bd15ead76d6e0ebdfe7c
Soumitra <projwebinfo@gmail.com>
2019-09-12 12:45:03 +0530

    renamed as jeejava.com moved to roytuts.com

---
+++ drools-support-spring-boot-2/src/main/java/com/roytuts/spring/drools/SpringBoot2Drools.java
@@ -0,0 +1,13 @@
+package com.roytuts.spring.drools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.roytuts.spring.drools")
+public class SpringBoot2Drools {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot2Drools.class, args);
+	}
+
+}
---
+++ drools-support-spring-boot-2/src/main/java/com/roytuts/spring/drools/vo/AreaPin.java
@@ -0,0 +1,15 @@
+package com.roytuts.spring.drools.vo;
+
+public class AreaPin {
+
+	private int code;
+
+	public AreaPin(int code) {
+		this.code = code;
+	}
+
+	public int getCode() {
+		return code;
+	}
+
+}
