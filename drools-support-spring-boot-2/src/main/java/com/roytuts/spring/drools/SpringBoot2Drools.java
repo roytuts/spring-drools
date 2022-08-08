7e3b76f18df3d53f8878e443d692c6595b3f561b
Soumitra <projwebinfo@gmail.com>
2022-08-08 17:10:32 +0530

    Update SpringBoot2Drools.java

--- drools-support-spring-boot-2/src/main/java/com/roytuts/spring/drools/SpringBoot2Drools.java
+++ drools-support-spring-boot-2/src/main/java/com/roytuts/spring/drools/SpringBoot2Drools.java
@@ -3,7 +3,7 @@ package com.roytuts.spring.drools;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.roytuts.spring.drools")
+@SpringBootApplication
 public class SpringBoot2Drools {
 
 	public static void main(String[] args) {
