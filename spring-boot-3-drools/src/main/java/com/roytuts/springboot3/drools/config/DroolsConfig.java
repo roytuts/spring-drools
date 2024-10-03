76bc043b821fdaedd2ce2281a37c85720de4899c
Soumitra <projwebinfo@gmail.com>
2024-10-03 17:41:11 +0530

    Create DroolsConfig.java

---
+++ spring-boot-3-drools/src/main/java/com/roytuts/springboot3/drools/config/DroolsConfig.java
@@ -0,0 +1,70 @@
+package com.roytuts.springboot3.drools.config;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import org.kie.api.KieServices;
+import org.kie.api.builder.KieBuilder;
+import org.kie.api.builder.KieFileSystem;
+import org.kie.api.builder.KieModule;
+import org.kie.api.builder.KieRepository;
+import org.kie.api.builder.Message;
+import org.kie.api.builder.ReleaseId;
+import org.kie.api.builder.Results;
+import org.kie.api.runtime.KieContainer;
+import org.kie.internal.io.ResourceFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
+
+@Configuration
+public class DroolsConfig {
+
+	private static final String RULES_PATH = "rules/";
+
+	@Bean
+	public KieFileSystem kieFileSystem() throws IOException {
+		KieFileSystem kieFileSystem = getKieServices().newKieFileSystem();
+
+		for (Resource file : getRuleFiles()) {
+			kieFileSystem.write(ResourceFactory.newClassPathResource(RULES_PATH + file.getFilename(),
+					StandardCharsets.UTF_8.name()));
+		}
+
+		return kieFileSystem;
+	}
+
+	private Resource[] getRuleFiles() throws IOException {
+		ResourcePatternResolver resourcePatternResolver = new PathMatchingResourcePatternResolver();
+		return resourcePatternResolver.getResources("classpath*:" + RULES_PATH + "**/*.*");
+	}
+
+	@Bean
+	public KieContainer kieContainer() throws IOException {
+		final KieRepository kieRepository = getKieServices().getRepository();
+
+		kieRepository.addKieModule(new KieModule() {
+			public ReleaseId getReleaseId() {
+				return kieRepository.getDefaultReleaseId();
+			}
+		});
+
+		KieBuilder kieBuilder = getKieServices().newKieBuilder(kieFileSystem()).buildAll();
+
+		Results results = kieBuilder.getResults();
+
+		if (results.hasMessages(Message.Level.ERROR)) {
+			System.out.println(results.getMessages());
+			throw new IllegalStateException("### errors ###");
+		}
+
+		return getKieServices().newKieContainer(kieRepository.getDefaultReleaseId());
+	}
+
+	private KieServices getKieServices() {
+		return KieServices.Factory.get();
+	}
+
+}
