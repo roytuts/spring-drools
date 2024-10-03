c0d008a0942cc3d6e58d41ce9936111ea2444487
Soumitra <projwebinfo@gmail.com>
2024-10-03 17:42:07 +0530

    Create Employee.java

---
+++ spring-boot-3-drools/src/main/java/com/roytuts/springboot3/drools/dto/Employee.java
@@ -0,0 +1,57 @@
+package com.roytuts.springboot3.drools.dto;
+
+public class Employee {
+
+	private long id;
+	private String name;
+	private String email;
+	private long phone;
+	private Address address;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public long getPhone() {
+		return phone;
+	}
+
+	public void setPhone(long phone) {
+		this.phone = phone;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", email=" + email + ", phone=" + phone + ", address=["
+				+ address.toString() + "]]";
+	}
+
+}
