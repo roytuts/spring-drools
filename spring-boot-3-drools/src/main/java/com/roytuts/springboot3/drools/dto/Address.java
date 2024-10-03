d56873ae79c1a580c19dec05d634019783c76af8
Soumitra <projwebinfo@gmail.com>
2024-10-03 17:41:40 +0530

    Create Address.java

---
+++ spring-boot-3-drools/src/main/java/com/roytuts/springboot3/drools/dto/Address.java
@@ -0,0 +1,66 @@
+package com.roytuts.springboot3.drools.dto;
+
+public class Address {
+
+	private String flatNo;
+	private String street;
+	private String city;
+	private String state;
+	private int zip;
+	private String country;
+
+	public String getFlatNo() {
+		return flatNo;
+	}
+
+	public void setFlatNo(String flatNo) {
+		this.flatNo = flatNo;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public int getZip() {
+		return zip;
+	}
+
+	public void setZip(int zip) {
+		this.zip = zip;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	@Override
+	public String toString() {
+		return "Address [flatNo=" + flatNo + ", street=" + street + ", city=" + city + ", state=" + state + ", zip="
+				+ zip + ", country=" + country + "]";
+	}
+
+}
