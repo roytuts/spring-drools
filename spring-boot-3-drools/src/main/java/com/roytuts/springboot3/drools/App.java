e5bdd60dad3342d31449eba3038389ae44865ec6
Soumitra <projwebinfo@gmail.com>
2024-10-03 17:40:28 +0530

    Create App.java

---
+++ spring-boot-3-drools/src/main/java/com/roytuts/springboot3/drools/App.java
@@ -0,0 +1,13 @@
+package com.roytuts.springboot3.drools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+
+}
